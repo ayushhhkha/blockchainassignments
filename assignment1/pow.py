fdc8786fa7094c527b6bf9c1fb6af8419228b52d
Ayush <ayushkhadkak@gmail.com>
2026-05-01 11:02:55 +0200

    proof of work added

---
+++ assignment1/pow.py
@@ -0,0 +1,21 @@
+import hashlib
+import struct
+
+email = "ayushkhadka@student.tudelft.nl"
+github_url = "https://github.com/ayushhhkha/blockchainassignments"
+
+prefix = email.encode() + b"\n" + github_url.encode() + b"\n"
+
+nonce = 0
+
+while True:
+    nonce_bytes = struct.pack(">Q", nonce)
+    h = hashlib.sha256(prefix + nonce_bytes).digest()
+
+    if h[:3] == b'\x00\x00\x00' and h[3] < 16:
+        print("SUCCESS")
+        print("Nonce:", nonce)
+        print("Hash:", h.hex())
+        break
+
+    nonce += 1
